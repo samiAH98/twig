ce000beca875623ae5429c78ef453ff179d4c91c
Ahamadi Sami <sami.ahamadi@laposte.net>
2024-01-02 17:00:03 +0100

    twig1

---
+++ public/products.php
@@ -0,0 +1,6 @@
+<?php
+
+global $twig;
+require_once __DIR__ . '/../config/twig.php';
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
+echo $twig->render('products.html.twig', ['product' => $products]);
